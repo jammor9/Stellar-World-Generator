295a28e38eb54ef1b26e87ae163c8d31d970eff4
jammor9 <jamesamorrison2005@googlemail.com>
2024-01-10 12:18:02 +0000

    main

---
+++ GUI.py
@@ -0,0 +1,108 @@
+import pygame as pg
+from pygame.locals import *
+
+import numpy as np
+from numpy import random
+
+def load_gui(world, size):
+
+    file = 'Graphics/StarMap.png'
+    TILE_SIZE = 16
+
+    class Game:
+        W = size * TILE_SIZE
+        H = size * TILE_SIZE
+        SIZE = W, H
+
+        def __init__(self):
+            pg.init()
+            self.screen = pg.display.set_mode(Game.SIZE)
+            pg.display.set_caption('Star Map')
+            self.running = True
+
+        def run(self):
+            while self.running:
+                for event in pg.event.get():
+                    if event.type == QUIT:
+                        self.running = False
+                    elif event.type == KEYDOWN:
+                        if event.key == K_1:
+                            self.load_image(file)
+
+            pg.quit()
+
+        def load_image(self, file):
+            self.file = file
+            self.image = pg.image.load(file)
+            self.rect = self.image.get_rect()
+
+            self.screen = pg.display.set_mode(self.rect.size)
+            pg.display.set_caption(f'size{self.rect.size}')
+            self.screen.blit(self.image, self.rect)
+            pg.display.update()
+
+    class Tileset:
+        def __init__(self, file, size=(TILE_SIZE, TILE_SIZE), margin=0, spacing=0):
+            self.file = file
+            self.size = size
+            self.margin = margin
+            self.spacing = spacing
+            self.image = pg.image.load(file)
+            self.rect = self.image.get_rect()
+            self.tiles = []
+            self.load()
+
+        def load(self):
+            self.tiles = []
+            x0 = y0 = self.margin
+            w, h = self.rect.size
+            dx = self.size[0] + self.spacing
+            dy = self.size[1] + self.spacing
+
+            for x in range(x0, w, dx):
+                for y in range(y0, h, dy):
+                    tile = pg.surface(self.size)
+                    tile.blit(self.image, (0, 0), (x, y, *self.size))
+                    self.tiles.append(tile)
+
+        def __str__(self):
+            return f'{self.__class__.__name__} file:{self.file} tile:{self.size}'
+
+    class TileMap:
+        def __init__(self, tileset, size=(10, 20), rect=None):
+            self.size = size
+            self.tileset = tileset
+            self.map = np.zeros(size, dtype=int)
+
+            h, w = self.size
+            self.image = pg.Surface((TILE_SIZE*w, TILE_SIZE*h))
+            if rect:
+                self.rect = pg.Rect(rect)
+            else:
+                self.rect = self.image.get_rect()
+
+        def render(self):
+            m, n = self.map.shape
+            for i in range(m):
+                for j in range(n):
+                    tile = self.tileset.tiles[self.map[i, j]]
+                    self.image.blit(tile, (j*TILE_SIZE, i*TILE_SIZE))
+
+        def set_zero(self):
+            self.map = np.zeros(self.size, dtype=int)
+            print(self.map)
+            print(self.map.shape())
+            self.render()
+
+        def set_random(self):
+            n = len(self.tileset.tiles)
+            self.map = random.randint(n, size=self.size)
+            print(self.map)
+            self.render()
+
+        def __str__(self):
+            return f'{self.__class__.__name__} {self.size}'
+
+
+    game = Game()
+    game.run()
--- Run.py
+++ Run.py
@@ -4,7 +4,8 @@ import numpy as np
 import tkinter as tk
 from tkinter import ttk
 
-from PIL import Image
+import pygame as pg
+from pygame.locals import *
 
 import matplotlib.pyplot as plt
 from matplotlib import colors
@@ -13,13 +14,14 @@ from matplotlib import colors
 import WorldGen as wg
 import Save as save
 import Classes as hc
+import GUI
 
 #Global Variables
 star_names = {0:"Space",1:"Verdant",2:"Primeval",3:"Stone",4:"Submerged",5:"Dry",6:"Tropical",7:"Frosty",
                   8:"Arcane",9:"Crimson",10:"Tyrian",11:"Golden"}
 
 star_color = {0:'#000000',1:'#6cf542',2:'#243d1d',3:'#5e5b55',4:'#223b7a',5:'#7a6f41', 
-            6:'#3f4d25',7:'#34a8a8',8:'#41ba65',9:'#590c22',10:'#4e0e63',11:'#e0b710'}
+            6:'#3f4d25',7:'#34a8a8',8:' ',9:'#590c22',10:'#4e0e63',11:'#e0b710'}
 
 char_type = {0:"Leader",1:"Adventurer",2:"Scholar"}
 
@@ -52,8 +54,8 @@ def generate_world():
     
     #Saves the simulation and displays resulting grid
     #save.save_data(world, star_info, civ_info, char_info, settlement_info, deity_info, pantheon_info, save_name)
-    tkinter_grid(world, len(world))
-    #display_image(world, size)
+    #tkinter_grid(world, len(world))
+    GUI.load_gui(world, len(world))
     
     return
 
@@ -109,6 +111,7 @@ def tkinter_grid(world, size):
 
     #Calls the window
     window.mainloop()
+
 
 generate_world()
 
